0e8ace072cbeed6500186b478e26d0e6c9070b18
amy vetters <amyvetters@gmail.com>
2022-05-03 11:28:15 -0400

    Add project files.

---
+++ Application.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace HW3_Fractions
+{
+    class Application
+    {
+        public static void Main(string[] args)
+        {
+            string choice = "";
+            Fraction f1 = new Fraction();
+            Fraction f2 = new Fraction();
+
+            while (choice.ToUpper() != "X")
+            {
+
+                System.Console.WriteLine("*******************************************");
+                System.Console.WriteLine("Please make your selection");
+                System.Console.WriteLine();
+                System.Console.WriteLine("Enter first fraction (1)");
+                System.Console.WriteLine("Enter second fraction (2)");
+                System.Console.WriteLine();
+                System.Console.WriteLine("(A)ddtion");
+                System.Console.WriteLine("(S)ubtraction");
+                System.Console.WriteLine("(M)ultiplication");
+                System.Console.WriteLine("(D)ivision");
+                System.Console.WriteLine("(G)reater than");
+                System.Console.WriteLine("(L)ess than");
+                System.Console.WriteLine("(E)qual to");
+                System.Console.WriteLine();
+                System.Console.WriteLine("e(X)it");
+                System.Console.WriteLine("*******************************************");
+                System.Console.WriteLine();
+                choice = System.Console.ReadLine();
+
+                if (choice == "1" || choice == "2")
+                {
+                    System.Console.Write("Enter the numerator: ");
+                    int numerator = Int32.Parse(System.Console.ReadLine());
+                    System.Console.Write("Enter the denominator: ");
+                    int denominator = Int32.Parse(System.Console.ReadLine());
+                    if (choice == "1")
+                        f1 = new Fraction(numerator, denominator);
+                    else
+                        f2 = new Fraction(numerator, denominator);
+                
+                }
+                else if (choice.ToUpper() == "A")
+                {
+                    System.Console.WriteLine(f1 + " + " + f2 + " = " + (f1 + f2));
+                }
+                else if (choice.ToUpper() == "S")
+                {
+                    System.Console.WriteLine(f1 + " - " + f2 + " = " + (f1 - f2));
+                }
+                else if (choice.ToUpper() == "M")
+                {
+                    System.Console.WriteLine(f1 + " × " + f2 + " = " + (f1 * f2));
+                }
+                else if (choice.ToUpper() == "D")
+                {
+                    try
+                    {
+                        System.Console.WriteLine(f1 + " ÷ " + f2 + " = " + (f1 / f2));
+                    }
+                    catch (DivideByZeroException dbze)
+                    {
+                        System.Console.WriteLine("ERROR: " + dbze.Message + "\n  Operands: " + f1 + ", " + f2);
+                    }
+                }
+                else if (choice.ToUpper() == "G")
+                {
+                    System.Console.WriteLine(f1 + ">" + f2 + ": " + (f1 > f2));
+                }
+                else if (choice.ToUpper() == "L")
+                {
+                    System.Console.WriteLine(f1 + "<" + f2 + ": " + (f1 < f2));
+                }
+                else if (choice.ToUpper() == "E")
+                {
+                    System.Console.WriteLine(f1 + "=" + f2 + ": " + (f1.Equals(f2)));
+                }
+                System.Console.WriteLine();
+                
+            }
+        }
+    }
+}
---
+++ Fraction.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+namespace HW3_Fractions
+
+{
+    public class Fraction
+    {
+        int Numerator { get; set; }
+        int Denominator { get; set; }
+
+        //set fraction
+        public Fraction()
+        {
+            Numerator = 0;
+            Denominator = 1;
+        }
+
+        //set numerator and denominator as 1 to create a whole number
+        public Fraction(int wholeNumber)
+        {
+            Numerator = wholeNumber;
+            Denominator = 1;
+        }
+
+        //create fraction with given numerator and denominator
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            Reduce();
+        }
+
+        //calls back fraction in reduced form (2/4 -> 1/2)
+        private void Reduce()
+        {
+            int commonDenom = GCD(this.Numerator, this.Denominator);
+            this.Numerator /= commonDenom;
+            this.Denominator /= commonDenom;
+        }
+
+        /* The following helper functions are provided for you. */
+        private static int GCD(int n, int m)
+        {
+            while (n != 0 && m != 0)
+            {
+                if (n > m)
+                    n = n % m;
+                else
+                    m = m % n;
+            }
+            return Math.Max(n, m);
+        }
+
+        private static int LCM(int n, int m)
+        {
+            return n * m / GCD(n, m);
+        }
+
+
+        //overloaded logical operators
+
+        public override bool Equals(object obj)
+        {
+
+            if (obj == null)
+            {
+                return false;
+            }
+            else if ((this.Numerator != ((Fraction)obj).Numerator) || (this.Denominator != ((Fraction)obj).Denominator))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+
+
+
+        //equality
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+            if (f1.Equals(f2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            if (!(f1.Equals(f2)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //operations
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            int commonDenom = LCM(f1.Denominator, f2.Denominator);
+            Fraction result = new Fraction();
+            if (f1.Denominator == f2.Denominator)
+            {
+                result.Numerator = f1.Numerator + f2.Numerator;
+                result.Denominator = f1.Denominator;
+
+            }
+            else
+            {
+                result.Numerator = f1.Numerator * (commonDenom / f1.Denominator) + f2.Numerator * (commonDenom / f2.Denominator);
+                result.Denominator = commonDenom;
+            }
+            return result;
+            
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            int commonDenom = LCM(f1.Denominator, f2.Denominator);
+            Fraction result = new Fraction();
+            if (f1.Denominator == f2.Denominator)
+            {
+                result.Numerator = f1.Numerator - f2.Numerator;
+                result.Denominator = f1.Denominator;
+
+            }
+            else
+            {
+                result.Numerator = f1.Numerator * (commonDenom / f1.Denominator) - f2.Numerator * (commonDenom / f2.Denominator);
+                result.Denominator = commonDenom;
+            }
+            return result;
+
+        }
+
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction();
+            result.Numerator = f1.Numerator * f2.Numerator;
+            result.Denominator = f1.Denominator * f2.Denominator;
+
+            return result;
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            Fraction result = new Fraction();
+            result.Numerator = f1.Numerator * f2.Denominator;
+            result.Denominator = f1.Denominator * f2.Numerator;
+
+            return result;
+        }
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            int commonDenom = LCM(f1.Denominator, f2.Denominator);
+            if ((f1.Numerator * (commonDenom / f1.Denominator)) < (f2.Numerator * (commonDenom / f2.Denominator)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            int commonDenom = LCM(f1.Denominator, f2.Denominator);
+            if ((f1.Numerator * (commonDenom / f1.Denominator)) > (f2.Numerator * (commonDenom / f2.Denominator)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "( " + this.Numerator + " / " + this.Denominator + " )";
+        }
+
+    }
+}
